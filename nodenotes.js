7a702d040d3f57bdeb7a07ff049d88e540cdbce0
Alek Lucatero <lucaalek99@gmail.com>
2024-03-30 22:03:18 -0700

    Serving a file on a get HTTP req
    In this example I server a file on app.get request. I do this by using the res.sendFile. __dirname is the global variable for the current directory or file path that the user is on. The concatenation with the string is the location of the file that we want to send.

--- nodenotes.js
+++ nodenotes.js
@@ -5,6 +5,11 @@ let express = require('express');
 let app = express();
 
 //Serving a string
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+// });
+
+//Serving a file. Useful when we want to show pictures etc.
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
